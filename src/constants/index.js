35c4e102ffdb1efb92039c9563c98f1d5e4afa97
UnicornDad8 <cecibenitezca@gmail.com>
2026-06-08 14:48:40 -0300

    Start Hero section

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { text: "Ideas", imagePath: "/images/ideas.svg" },
+  { text: "Concepts", imagePath: "/images/concepts.svg" },
+  { text: "Designs", imagePath: "/images/designs.svg" },
+  { text: "Code", imagePath: "/images/code.svg" },
+  { text: "Ideas", imagePath: "/images/ideas.svg" },
+  { text: "Concepts", imagePath: "/images/concepts.svg" },
+  { text: "Designs", imagePath: "/images/designs.svg" },
+  { text: "Code", imagePath: "/images/code.svg" }
+]
