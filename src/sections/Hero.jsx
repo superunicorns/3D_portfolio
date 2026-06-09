5cea85e6ad79965944634b382530e95dd739d84e
UnicornDad8 <cecibenitezca@gmail.com>
2026-06-09 12:05:05 -0300

    Remove border from hero right figure

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
         </header>
 
         <figure>
-          <div className="hero-3d-layout border-red-200 border-2">
+          <div className="hero-3d-layout">
             <HeroExperience />
           </div>
         </figure>
