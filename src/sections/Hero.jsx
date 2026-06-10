8f4b199d78737c1874d6a7a5c6f605beee27fdae
UnicornDad8 <cecibenitezca@gmail.com>
2026-06-10 05:48:42 -0300

    Add AnimatedCounter component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
@@ -57,6 +58,7 @@ const Hero = () => {
           </div>
         </figure>
       </div>
+      <AnimatedCounter />
     </section>
   )
 }
