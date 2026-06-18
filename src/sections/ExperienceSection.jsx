8e560c7f421b4c49b64c896c00a2bf4ac79f7295
UnicornDad8 <cecibenitezca@gmail.com>
2026-06-18 15:54:57 -0300

    Add GlowCard component

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,3 +1,4 @@
+import GlowCard from "../components/GlowCard";
 import TitleHeader from "../components/TitleHeader";
 import { expCards } from "../constants";
 
@@ -11,9 +12,15 @@ const ExperienceSection = () => {
         />
         <div className="mt-32 relative">
           <div className="relative z-50 xl:space-y-32 space-y-10">
-            {expCards.map((card) => (
+            {expCards.map((card, index) => (
               <div key={card.title} className="exp-card-wrapper">
-                {card.title}
+                <div className="xl:w-2/6">
+                  <GlowCard card={card} index={index}>
+                    <div>
+                      <img src={card.imgPath} alt={card.title} />
+                    </div>
+                  </GlowCard>
+                </div>
               </div>
             ))}
           </div>
