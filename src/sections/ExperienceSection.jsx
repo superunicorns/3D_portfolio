35f55012c901bda4f5ccbd10ed307728b90100f2
UnicornDad8 <cecibenitezca@gmail.com>
2026-06-18 16:27:11 -0300

    Add work experience right content

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -21,6 +21,31 @@ const ExperienceSection = () => {
                     </div>
                   </GlowCard>
                 </div>
+                <div className="xl:w-4/6">
+                  <div className="flex items-start">
+                    <div className="timeline-wrapper">
+                      <div className="timeline" />
+                      <div className="gradient-line w-1 h-full" />
+                    </div>
+                    <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+                      <div className="timeline-logo">
+                        <img src={card.logoPath} alt="logo" />
+                      </div>
+                      <div>
+                        <h1 className="font-semibold text-3xl">{card.title}</h1>
+                        <p className="my-5 text-white-50">🗓️ {card.date}</p>
+                        <p className="text-[#839cb5] italic">Responsibilities</p>
+                        <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
+                          {card.responsibilities.map((responsability) => (
+                            <li key={responsability} className="text-lg">
+                              {responsability}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
